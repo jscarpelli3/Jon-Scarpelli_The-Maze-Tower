deacac325fbab2bbbd3be4f92e064c95e390a9a0
Jon Scarpelli <scarpelli.jon@gmail.com>
2023-01-14 17:58:18 -0600

    html and js files intitialized with event listener for button

---
+++ level_creator.js
@@ -0,0 +1,6 @@
+const startButton = document.querySelector(`button`)
+
+///start button takes you to the game
+startButton.addEventListener(`click`, () => {
+  window.location.href = 'game.html'
+})
