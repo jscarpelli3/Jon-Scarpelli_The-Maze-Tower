57cabbac221af568908eb96f22b4ba4bd43074c9
Jon Scarpelli <scarpelli.jon@gmail.com>
2023-01-15 15:05:15 -0600

    added button on index, needs to move

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const startButton = document.querySelector(`button`)
+const startButton = document.querySelector(`.start`)
+const lvlCreateButton = document.querySelector(`.level-creator`)
 
 ///start button takes you to the game
 startButton.addEventListener(`click`, () => {
   window.location.href = 'game.html'
+})
+
+lvlCreateButton.addEventListener(`click`, () => {
+  window.location.href = 'level_creator.html'
 })
